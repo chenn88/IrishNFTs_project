b0ee163c3289dd2f7f38689480afc887c5a3b335
chenn88 <cp.hennessy@outlook.com>
2023-04-08 12:23:18 +0100

    updated order total to correct data format

--- Back-end/OrdersAPI/Models/Order.cs
+++ Back-end/OrdersAPI/Models/Order.cs
@@ -8,7 +8,7 @@ namespace OrdersAPI.Models
         public int OrderId { get; set; }
         public int ProductId { get; set; }
         public DateTime OrderDate { get; set; }
-        public int OrderTotal { get; set; }
+        public decimal OrderTotal { get; set; }
         public bool OrderCancellation { get; set; }
     }
 }
--- Front-End/IrishNFTs.MVC/Models/OrderViewModel.cs
+++ Front-End/IrishNFTs.MVC/Models/OrderViewModel.cs
@@ -6,7 +6,7 @@ namespace IrishNFTs.MVC.Models
         public int OrderId { get; set; }
         public int ProductId { get; set; }
         public DateTime OrderDate { get; set; }
-        public int OrderTotal { get; set; }
+        public decimal OrderTotal { get; set; }
         public bool OrderCancellation { get; set; }
 
 
